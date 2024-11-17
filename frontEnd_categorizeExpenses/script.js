ecb34af1edbbae26df66e136cae65cf8bb224138
Vianca Varma <63786369+viancavarma@users.noreply.github.com>
2024-11-17 19:35:52 +0000

    basic sript.js

---
+++ frontEnd_categorizeExpenses/script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const expenseLabel = document.getElementById('expenseLabel');
+    const expenseAmount = document.getElementById('expenseAmount');
+    const expenseCategory = document.getElementById('expenseCategory');
+    const submitExpense = document.getElementById('submitExpense');
+    const expenseLogsTable = document.querySelector('#expenseLogs tbody');
+    const logsView = document.getElementById('logsView');
+
+    
+});
