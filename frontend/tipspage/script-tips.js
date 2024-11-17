68959ba3db51cefe82d7818a7f82495ca7b9980d
kaylinrutkowski <krutkowski@umass.edu>
2024-11-17 13:33:15 -0500

    update: add functionality to get the expenses once input by user for brief summary

--- frontend/tipspage/script-tips.js
+++ frontend/tipspage/script-tips.js
@@ -1,5 +1,5 @@
 // list of different tips that are helpful to financial wellness
-const allTips = ['50-30-20 Rule: 50% of paycheck should go to regular expenses; 30% should go to personal expenses; 20% should go to savings',
+const allTips = ['50-30-20 Rule: 50% of paycheck -> regular expenses; 30% -> personal expenses; 20% -> savings',
     'Go shopping with a list to prevent overspending',
     "Don't succumb to the instant gratification of spending (think it over)",
     'Track all of your spending in our app',
@@ -12,6 +12,7 @@ const allTips = ['50-30-20 Rule: 50% of paycheck should go to regular expenses; 
     "Limit your 'want' purchases",
     'Take advantage of free public resources'
 ]
+
 
 
 // shuffle the list of tips
@@ -54,6 +55,8 @@ Date.prototype.getWeekNumber = function () {
     const numberOfDays = Math.floor((this - oneJan) / (24 * 60 * 60 * 1000));
     return Math.ceil((numberOfDays + oneJan.getDay() + 1) / 7);
 };
+
+
 
 
 displayTips();
