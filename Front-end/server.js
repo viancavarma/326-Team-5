f478ae2618ba96f9835c41a90db05807e6f4514b
kaylinrutkowski <krutkowski@umass.edu>
2024-12-03 13:51:35 -0500

    most expensive expense #7: added mock directory, to test backend retrieval

--- Front-end/server.js
+++ Front-end/server.js
@@ -3,6 +3,9 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
+// create a mock directory to serve as database
+app.use(express.static("mock"));
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
