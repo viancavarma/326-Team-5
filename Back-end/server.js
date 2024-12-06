f6408c8155685661291c0c3391cbe4878222c450
kaylinrutkowski <krutkowski@umass.edu>
2024-12-06 11:37:14 -0500

    most expensive expense #7: added server.js, implement get and fetch for most expensive expense

--- Back-end/server.js
+++ Back-end/server.js
@@ -3,9 +3,21 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
-// create a mock directory to serve as database
-app.use(express.static("mock"));
+// use mock data
+const mockData = require('./mockData.json');
 
+// Route to get the most expensive item
+app.get('/api/most-expensive-item', (req, res) => {
+  const mostExpensiveItem = mockData.reduce((max, item) => {
+    const itemAmount = parseFloat(item.amount.replace('$', ''));
+    const maxAmount = parseFloat(max.amount.replace('$', ''));
+    return itemAmount > maxAmount ? item : max;
+  });
+
+  res.json(mostExpensiveItem); // Respond with the most expensive item
+});
+
+// Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
