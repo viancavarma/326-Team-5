003c92c014b431d2e6c05ee19ddc2c2c14be2cf2
PawanT123 <167481307+PawanT123@users.noreply.github.com>
2024-12-10 14:53:54 -0500

    Update SQLiteNotificationModel.js
    sequele

---
+++ Back-end/models/SQLiteNotificationModel.js
@@ -0,0 +1,11 @@
+import { DataTypes } from 'sequelize';
+import sequelize from './database.js';
+
+const Notification = sequelize.define('Notification', {
+    id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+    userId: { type: DataTypes.INTEGER, allowNull: false },
+    message: { type: DataTypes.STRING, allowNull: false },
+    read: { type: DataTypes.BOOLEAN, defaultValue: false },
+    createdAt: { type: DataTypes.DATE, defaultValue: DataTypes.NOW }
+});
+
